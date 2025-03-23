7ded2211dd7217590822a9a8c170d841fc1cfb17
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-03-23 10:47:44 -0300

    Initial commit

---
+++ test/ecomerce_aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcomerceAws from '../lib/ecomerce_aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecomerce_aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcomerceAws.EcomerceAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
