e5c128d983a66fac1874eee5e4644765404a866b
Gabriel Jesus <gabrieldejesus16gs@gmail.com>
2025-04-11 07:37:31 -0300

    feat: instace layer

--- bin/ecomerce_aws.ts
+++ bin/ecomerce_aws.ts
@@ -3,8 +3,10 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ProductsAppStack } from '../lib/productsApp-stack'
 import { EcomerceApiStack } from '../lib/ecommerceApi-stack'
+import { ProductsAppLayersStack } from '../lib/productsAppLayers-stack'
 
 const app = new cdk.App();
+
 const env: cdk.Environment = {
   account: "442754113624",
   region: "us-east-1",
@@ -14,11 +16,17 @@ const tags = {
   cost: "ecommerce",
   team: "GJ",
 }
+const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayersStack", {
+  tags,
+  env
+})
 
 const productsAppStack = new ProductsAppStack(app, 'ProductsAppStack', {
   tags,
   env
 })
+
+productsAppStack.addDependency(productsAppLayersStack)
 
 const ecommerceApiStack = new EcomerceApiStack(app, 'EcommerceApiStack', {
   productsFetchHandler: productsAppStack.productsFetchHandler,
